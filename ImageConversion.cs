00c0f456848a72af0507cf968b4d2e2b4fc0e9f0
HunterSun2018 <HunterSun2018@gmail.com>
2024-04-09 14:19:23 +0800

    first commit

---
+++ ImageConversion.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using StableDiffusion.NET.Helper.Images.Colors;
+using StableDiffusion.NET.Helper.Images;
+using SkiaSharp;
+
+public static class ImageConversion
+{
+    public static unsafe SKBitmap ToBitmap(this RefImage<ColorRGB> image)
+    {
+        SKBitmap bitmap = new SKBitmap(image.Width, image.Height);
+        IntPtr pixels = bitmap.GetPixels();
+        byte* ptr = (byte*)pixels.ToPointer();
+        var info = bitmap.Info;
+
+        foreach (ReadOnlyRefEnumerable<ColorRGB> row in image.Rows)
+        {
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                ColorRGB srcColor = row[i];
+
+                *ptr++ = srcColor.R;   // red
+                *ptr++ = srcColor.G;   // green
+                *ptr++ = srcColor.B;   // blue
+                *ptr++ = 0xFF;         // alpha
+            }
+        }
+
+        return bitmap;
+    }
+
+    public static byte[] ToPng(this IImage image)
+    {
+        using SKBitmap bitmap = ToBitmap(image.AsRefImage<ColorRGB>());
+
+        MemoryStream wStream = new MemoryStream();
+        using (SKManagedWStream wstream = new SKManagedWStream(wStream))
+        {
+            bitmap.Encode(wstream, SKEncodedImageFormat.Png, 100);
+            byte[] bytes = wStream.ToArray();
+
+            return bytes;
+        }
+    }
+
+    
+    public static async Task<byte[]> test()
+    {
+        string url = "http://120.133.36.29:8081/1712550685.png";
+        HttpClient httpClient = new HttpClient();
+
+        try
+        {
+            using (Stream stream = await httpClient.GetStreamAsync(url))
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memStream);
+                memStream.Seek(0, SeekOrigin.Begin);
+
+                SKBitmap webBitmap = SKBitmap.Decode(memStream);
+                // canvasView.InvalidateSurface();
+
+                using (MemoryStream wStream = new MemoryStream())
+                using (SKManagedWStream wstream = new SKManagedWStream(wStream))
+                {
+                    webBitmap.Encode(wstream, SKEncodedImageFormat.Png, 100);
+                    byte[] bytes = wStream.ToArray();
+
+                    return bytes;
+                }
+            };
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using StableDiffusion.NET;
+using StableDiffusion.NET.Helper.Images;
+using StableDiffusion.NET.Helper.Images.Colors;
+
+Console.WriteLine("Hello, World!");
+string vaePath = "/home/hunter/Projects/cpp/ai/stable-diffusion.cpp/models/sdxl.vae.safetensors";
+string modelPath = "/home/hunter/Projects/cpp/ai/stable-diffusion.cpp/models/sd_xl_base_1.0.safetensors";
+
+using StableDiffusionModel sd = new(modelPath, new ModelParameter { VaePath = vaePath });
+using StableDiffusionImage image = sd.TextToImage("retail packaging style Nike Shoes. vibrant, enticing, commercial, product-focused, eye-catching, professional, highly detailed",
+                                    new StableDiffusionParameter
+                                    {
+                                        Width = 1024,
+                                        Height = 1024,
+                                        NegativePrompt = "noisy, blurry, amateurish, sloppy, unattractive",
+                                        Seed = 1712560398,
+                                        SampleMethod = Sampler.Euler,
+                                        SampleSteps = 20
+                                    });
+
+IImage? img = image.ToImage();
+
+byte[] data = ImageConversion.ToPng(img);
+File.WriteAllBytes("test.png", data);
